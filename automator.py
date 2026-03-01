84703d69b030884de20f0b1922cae5abe8e24372
Anirudh Bagri <anirudhbagri@users.noreply.github.com>
2026-03-01 17:22:44 +0530

    Refactor and fix the send button

--- automator.py
+++ automator.py
@@ -1,90 +1,104 @@
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from time import sleep
 from urllib.parse import quote
 import os
 
-options = Options()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-options.add_argument("--profile-directory=Default")
-options.add_argument("--user-data-dir=/var/tmp/chrome_user_data")
+DELAY = 30
 
-os.system("")
-os.environ["WDM_LOG_LEVEL"] = "0"
-class style():
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
+def get_user_data_dir():
+    """Returns the default user data directory path based on the operating system."""
+    if os.name == 'nt':  # Windows
+        return os.path.join(os.environ['LOCALAPPDATA'], 'AnirudhBagri', 'WABulker', 'User Data')
+    elif os.name == 'posix': 
+        if os.uname()[0] == 'Darwin': 
+            return os.path.join(os.environ['HOME'], 'Library', 'AnirudhBagri', 'WABulker', 'User Data')
+        else: 
+            return os.path.join(os.environ['HOME'], '.config', 'anirudhbagri', 'wabulker', 'user_data')
+    else:
+        raise Exception("Unsupported operating system")
 
-print(style.BLUE)
-print("**********************************************************")
-print("**********************************************************")
-print("*****                                               ******")
-print("*****  THANK YOU FOR USING WHATSAPP BULK MESSENGER  ******")
-print("*****      This tool was built by Anirudh Bagri     ******")
-print("*****           www.github.com/anirudhbagri         ******")
-print("*****                                               ******")
-print("**********************************************************")
-print("**********************************************************")
-print(style.RESET)
+def send_messages(driver, numbers, message):
+	for idx, number in enumerate(numbers):
+		number = number.strip()
+		if number == "":
+			continue
+		print('\n{}/{} => Sending message to {}.'.format((idx+1), len(numbers), number))
+		try:
+			url = 'https://web.whatsapp.com/send?phone=' + number + '&text=' + message
+			sent = False
+			for i in range(3):
+				if not sent:
+					driver.get(url)
+					try:
+						click_btn = WebDriverWait(driver, DELAY).until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Send']")))
+					except Exception as e:
+						print(f"\nFailed to send message to: {number}, retry ({i+1}/3)")
+						print("Make sure your phone and computer is connected to the internet.")
+						print("If there is an alert, please dismiss it.")
+					else:
+						sleep(1)
+						click_btn.click()
+						sent=True
+						sleep(3)
+						print(f'Message sent to: {number}')
+		except Exception as e:
+			print(f'Failed to send message to {number}: {e}')
 
-f = open("message.txt", "r", encoding="utf8")
-message = f.read()
-f.close()
+def print_intro():
+	print("\n**********************************************************")
+	print("*****                                               ******")
+	print("*****  THANK YOU FOR USING WHATSAPP BULK MESSENGER  ******")
+	print("*****      This tool was built by Anirudh Bagri     ******")
+	print("*****           www.github.com/anirudhbagri         ******")
+	print("*****                                               ******")
+	print("**********************************************************")
 
-print(style.YELLOW + '\nThis is your message-')
-print(style.GREEN + message)
-print("\n" + style.RESET)
-message = quote(message)
+def get_message() -> str:
+	f = open("message.txt", "r", encoding="utf8")
+	message = f.read()
+	f.close()
+	print('\nThis is your message-')
+	print(message)
+	message = quote(message)
+	return message
 
-numbers = []
-f = open("numbers.txt", "r")
-for line in f.read().splitlines():
-	if line.strip() != "":
-		numbers.append(line.strip())
-f.close()
-total_number=len(numbers)
-print(style.RED + 'We found ' + str(total_number) + ' numbers in the file' + style.RESET)
-delay = 30
+def get_numbers() -> list:
+	numbers = []
+	f = open("numbers.txt", "r")
+	for line in f.read().splitlines():
+		if line.strip() != "":
+			numbers.append(line.strip())
+	f.close()
+	return numbers
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-print('Once your browser opens up sign in to web whatsapp')
-driver.get('https://web.whatsapp.com')
-input(style.MAGENTA + "AFTER logging into Whatsapp Web is complete and your chats are visible, press ENTER..." + style.RESET)
-for idx, number in enumerate(numbers):
-	number = number.strip()
-	if number == "":
-		continue
-	print(style.YELLOW + '{}/{} => Sending message to {}.'.format((idx+1), total_number, number) + style.RESET)
-	try:
-		url = 'https://web.whatsapp.com/send?phone=' + number + '&text=' + message
-		sent = False
-		for i in range(3):
-			if not sent:
-				driver.get(url)
-				try:
-					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='compose-btn-send']")))
-				except Exception as e:
-					print(style.RED + f"\nFailed to send message to: {number}, retry ({i+1}/3)")
-					print("Make sure your phone and computer is connected to the internet.")
-					print("If there is an alert, please dismiss it." + style.RESET)
-				else:
-					sleep(1)
-					click_btn.click()
-					sent=True
-					sleep(3)
-					print(style.GREEN + 'Message sent to: ' + number + style.RESET)
-	except Exception as e:
-		print(style.RED + 'Failed to send message to ' + number + str(e) + style.RESET)
-driver.close()
+def get_driver():
+	options = Options()
+	options.add_experimental_option("excludeSwitches", ["enable-logging"])
+	options.add_argument("--profile-directory=Default")
+	options.add_argument("--user-data-dir=" + get_user_data_dir())
+	driver = webdriver.Chrome(service=Service(), options=options)
+	return driver
+
+def login_whatsapp(driver):
+	print('Once your browser opens up sign in to web whatsapp')
+	driver.get('https://web.whatsapp.com')
+	input("AFTER logging into Whatsapp Web is complete and your chats are visible, press ENTER...")
+
+def main():
+	print_intro()
+	message = get_message()
+	numbers = get_numbers()
+	print(f"\nFound {len(numbers)} numbers in the file.")
+	driver = get_driver()
+	login_whatsapp(driver)
+	send_messages(driver, numbers, message)
+	driver.close()
+	print("\nAll done! Thanks for using WhatsApp Bulk Messenger.")
+
+if __name__ == "__main__":
+	main()
